7b8edaa68dea0db45f4eb25cc4b0fae5617e5f8f
Gerhard <ggdeocampo@multisyscorp.com>
2021-06-04 15:29:17 +0800

    create test

---
+++ app/Biller/Cdv/Validators/BillerCodeTEST.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Biller\Cdv\Validators;
+
+use App\Exceptions\BillerValidatorException;
+use App\Biller\Cdv\Factory\BillerCdvInterface;
+
+class BillerCodeTEST implements BillerCdvInterface
+{
+    public function validate($mainField, $amount): bool
+    {
+        try {
+            // PNB Credit Cards
+            $mainField = preg_replace('/\D/', '', $mainField);
+            if ($amount < 10) {
+                return false;
+            }
+        } catch (\Throwable $th) {
+            throw new BillerValidatorException();
+        }
+
+        return true;
+    }
+}
