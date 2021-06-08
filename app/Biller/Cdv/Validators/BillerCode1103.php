bfc998d0c93b801fe79f5ae9ba989ea193dcb5cf
Gerhard <ggdeocampo@multisyscorp.com>
2021-06-08 16:22:40 +0800

    intialize branch

---
+++ app/Biller/Cdv/Validators/BillerCode1103.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Biller\Cdv\Validators;
+
+use App\Exceptions\BillerValidatorException;
+use App\Biller\Cdv\Factory\BillerCdvInterface;
+
+class BillerCode1103 implements BillerCdvInterface
+{
+    public function validate($mainField, $amount): bool
+    {
+        try {
+            $mainField = preg_replace('/\D/', '', $mainField);
+            if (
+                $this->validateLength($mainField) and 
+                $this->validateCharacters($mainField)
+            ) {
+                return true;
+            }
+        } catch (\Throwable $e) {
+            throw new BillerValidatorException();
+        }
+
+        return false;
+    }
+
+    private function validateLength($mainField)
+    {
+        $length = strlen($mainField);
+        if ($length < 7 or $length > 10) {
+            return false;
+        }
+        return true;
+    }
+
+    private function validateCharacters($mainField) {
+        return is_numeric($mainField);
+    } 
+}
