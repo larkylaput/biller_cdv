09a0aeb3ed1af3a46bfba3df554b780210703a9a
Gerald <gsanjose@centralizedinc.com>
2021-07-30 15:19:58 +0800

    billercode 82 has been added.

---
+++ app/Biller/Cdv/Validators/BillerCode82.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Biller\Cdv\Validators;
+
+use App\Exceptions\BillerValidatorException;
+use App\Biller\Cdv\Factory\BillerCdvInterface;
+
+class BillerCode82 implements BillerCdvInterface
+{
+    const weightVal = '1212';
+    const Divisor = 10;
+    public function validate($mainField, $amount): bool
+    {
+        
+        try {
+            // $mainField = preg_replace('/\D/', '', $mainField);
+            if (
+                $this->validateLength($mainField) AND 
+                $this->validateChars($mainField) AND 
+                $this->validateCharacters($mainField)
+            ) {
+                return true;
+            }
+        } catch (\Throwable $e) {
+            throw new BillerValidatorException();
+        }
+
+        return false;
+    }
+
+    private function validateLength($mainField)
+    {
+        $length = strlen($mainField);
+        
+        return $length === 5 ? true : false;
+    }
+
+    private function validateCharacters($mainField)
+    {
+        return is_numeric($mainField);
+    }
+
+    private function validateChars($mainField){
+        
+        $split_weight = str_split(self::weightVal);
+        $split_mainField = str_split(substr($mainField,0,4));
+        $LastDigit = substr($mainField,4,1);
+        $product = 0;
+        $total = 0;
+        $reimainder = 0;
+        foreach($split_mainField as $key => $data){
+
+            $product = $split_mainField[$key] * $data;
+
+            $total = $total + $product;
+
+        }
+        
+        $reimainder = fmod($total,self::Divisor);
+        
+        if($reimainder <> 0){
+            $total = self::Divisor - $reimainder;
+        }else{
+            $total = 0;
+        }
+
+        if($total == $LastDigit){
+            return true;
+        }else{
+            return false;
+        }
+
+
+    }       
+}
