6752ea081d52992be1885f166868c8e5adb5f1ce
reyesrlr <reyesrlr@pnb.com.ph>
2021-08-03 18:20:36 +0800

    biller-1004

---
+++ app/Biller/Cdv/Validators/BillerCode913.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Biller\Cdv\Validators;
+
+use App\Exceptions\BillerValidatorException;
+use App\Biller\Cdv\Factory\BillerCdvInterface;
+
+class BillerCode913 implements BillerCdvInterface
+{
+    public function validate($mainField, $amount): bool
+    {
+        try {
+            // $mainField = preg_replace('/\D/', '', $mainField);
+			// ABSCBN
+            if (
+                $this->validateLength($mainField) and 
+                $this->validateCharacters($mainField)
+            ) {
+                return true;
+            }
+        } catch (\Throwable $e) {
+            throw new BillerValidatorException();
+        }
+        return false;
+    }
+
+    private function validateLength($mainField)
+    {
+        $length = strlen($mainField);
+        if ($length === 50) {
+            return false;
+        }
+        return true;
+    }
+
+    private function validateCharacters($mainField) {
+        return($mainField);
+    } 
+
+}
+
