653cf4ce10ccd10da4674cb6cfab2a3210c45d46
Gerald <gsanjose@centralizedinc.com>
2021-08-05 10:43:35 +0800

    biller code 1104 has been added.

---
+++ app/Biller/Cdv/Validators/BillerCode1104.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Biller\Cdv\Validators;
+
+use App\Exceptions\BillerValidatorException;
+use App\Biller\Cdv\Factory\BillerCdvInterface;
+
+class BillerCode1104 implements BillerCdvInterface
+{
+    public function validate($mainField, $amount): bool
+    {
+        try {
+            $mainField = preg_replace('/\D/', '', $mainField);
+            if (
+                $this->validateLength($mainField) AND 
+                $this->validatechars($mainField)
+            ) {
+                return true;
+            }
+        } catch (\Throwable $e) {
+            throw new BillerValidatorException();
+        }
+
+        return false;
+    }
+
+    private function validateLength($mainField)
+    {
+        $length = strlen($mainField);
+        
+        return $length == 13 ? true : false;
+    }
+    
+    private function validateChars($mainField){
+
+        if(ctype_alpha(substr($mainField,0,1)) AND is_numeric(substr($mainField,1,13))){
+            return true;
+        }
+        return false;
+
+    }
+}
