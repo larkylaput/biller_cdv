fe60796eea01fe39bd91a83198adb0507adcbc63
Gerald <gsanjose@centralizedinc.com>
2021-08-10 12:09:50 +0800

    refactor of cdv, logic and condition from previous version has been changed based on updated CDV

--- app/Biller/Cdv/Validators/BillerCode1015.php
+++ app/Biller/Cdv/Validators/BillerCode1015.php
@@ -7,14 +7,19 @@ use App\Biller\Cdv\Factory\BillerCdvInterface;
 
 class BillerCode1015 implements BillerCdvInterface
 {
+    const given_string = ['CSH','CHK'];
+    const array_1 = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30'];
+    const array_2 = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
+    const array_3 = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29'];
     public function validate($mainField, $amount): bool
     {
         try {
             // $mainField = preg_replace('/\D/', '', $mainField);
             if (
                 $this->validateLength($mainField) AND 
+                $this->checkChars($mainField) AND
                 $this->validateCharacters($mainField) AND
-                $this->checkChars($mainField) 
+                $this->validateStrings($mainField) 
             ) {
                 return true;
             }
@@ -34,36 +39,40 @@ class BillerCode1015 implements BillerCdvInterface
 
     private function validateCharacters($mainField)
     {
-        dd(is_numeric(substr($mainField,10,7)) );
-        if(
-            ctype_alpha(substr($mainField,0,1)) OR
-            is_numeric(substr($mainField,1,6)) OR
-            ctype_upper(substr($mainField,7,3)) OR
-            is_numeric(substr($mainField,10,7)) 
-        ){
+ 
+        if (!preg_match('/%[0-9A-Z]%/',substr($mainField,0,3)))
+        {
             return true;
-
         }
-
         return false;
-        
+
+
     }
 
     private function checkChars($mainField){
      
-
-        if(intval(substr($mainField,3,4))  > 2000 AND intval(substr($mainField,3,4))  < 2050 ){ // year
-            if(intval(substr($mainField,10,2)) >= 1 AND intval(substr($mainField,10,2)) <= 12){ // month
-                if(intval(substr($mainField,12,2)) >= 1 AND intval(substr($mainField,12,2)) <= 31){ // day
-                    return true;
-                }else{
-                    return false;
-                }
+        if(intval(substr($mainField,3,4))  >= 1999 AND intval(substr($mainField,3,4))  <= 4999 AND in_array(substr($mainField,7,3),self::given_string)){ 
+            if(is_numeric(substr($mainField,14,3)) AND substr($mainField,14,3) != '000'){
+                return true;
             }else{
                 return false;
             }
         }else{
             return false;
         }
+    }
+
+    private function validateStrings($mainField){
+        if(in_array(substr($mainField,10,2),['04','06','09','11']) AND in_array(substr($mainField,10,2),self::array_1) ){
+            return true;
+        }else if(in_array(substr($mainField,10,2),['01','03','05','07','08','10','12']) AND in_array(substr($mainField,10,2),self::array_2) ){
+            return true;
+        }else if(substr($mainField,10,2) == '02' AND fmod(substr($mainField,3,4),4) == 0 AND in_array(substr($mainField,12,2),self::array_3) ){
+
+            return true;
+        }else{
+            return false;
+        }
+
     }
 }
