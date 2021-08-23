7ce025d5d98c5830f05faa5f2cccd940dc48125c
Jed Relator <jtrelator@multisyscorp.com>
2021-08-23 11:04:41 +0800

    feature/biller-1108

---
+++ app/Biller/Cdv/Validators/BillerCode1108.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Biller\Cdv\Validators;
+
+use App\Exceptions\BillerValidatorException;
+use App\Biller\Cdv\Factory\BillerCdvInterface;
+
+class BillerCode1108 implements BillerCdvInterface
+{
+    public function validate($mainField, $amount): bool
+    {
+        try {
+            if(
+                $this->validateLength($mainField) &&
+                $this->validateCharacters($mainField)
+            ) {
+                return true;
+            }
+
+        } catch (\Throwable $th) {
+            throw new BillerValidatorException();
+        }
+        return false;
+    }
+
+    private function validateLength($mainField) {
+        $length = strlen($mainField);
+        return ($length >= 8 && $length <= 12) ? true : false;
+    }
+
+    private function validateCharacters($mainField) {
+        if (!is_numeric(substr($mainField, 0, 6))) {
+            if (ctype_alnum(substr($mainField, 6, 6))) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+}
