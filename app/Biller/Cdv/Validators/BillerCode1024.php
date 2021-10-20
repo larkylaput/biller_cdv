510cf378509c8cd5b9005ab7b7cf44f07becfa73
Gerald <gsanjose@centralizedinc.com>
2021-10-20 13:49:34 +0800

    biller code 1024 has been added

---
+++ app/Biller/Cdv/Validators/BillerCode1024.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Biller\Cdv\Validators;
+
+use App\Exceptions\BillerValidatorException;
+use App\Biller\Cdv\Factory\BillerCdvInterface;
+
+class BillerCode1024 implements BillerCdvInterface
+{
+    public function validate($mainField, $amount): bool
+    {
+        // dd($this->validateCharacter($mainField));
+        try {
+            // $mainField = preg_replace('/\D/', '', $mainField);
+            if ($this->validateLength($mainField) &&
+                $this->validateCharacter($mainField) 
+            ) {
+                return true;
+            }
+        } catch (\Throwable $e) {
+            throw new BillerValidatorException();
+        }
+
+        return false;
+    }
+
+    private function validateLength($mainField) {
+        
+        if(strlen($mainField) > 9 && strlen($mainField) < 5){
+            return true;
+        }
+        
+        return false;
+    }
+
+    private function validateCharacter ($mainField) {
+        
+        $regex = preg_match('/[\'!^£$%&*()}{@#~?><>,|=_+¬-]/', $mainField);
+        if($regex)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+
+
+}
