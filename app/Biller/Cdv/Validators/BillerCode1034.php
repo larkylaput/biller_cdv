2097ce8986a49d0b4181c320805606c03ea19254
Jtrelator <jtrelator@multisys.com>
2022-02-28 10:21:52 +0800

    Modification of CDV of biller 01034 - LARC-Laguna Water District Aquatech in Digital App

--- app/Biller/Cdv/Validators/BillerCode1034.php
+++ app/Biller/Cdv/Validators/BillerCode1034.php
@@ -10,7 +10,9 @@ class BillerCode1034 implements BillerCdvInterface
     public function validate($mainField, $amount): bool
     {
         try {
-            return $this->validateLength($mainField) && $this->validateCharacters($mainField);
+            return $this->validateLength($mainField) && 
+            $this->validateCharacters($mainField) &&
+            $this->validateFormat($mainField);
         } catch (\Throwable $th) {
             throw new BillerValidatorException();
         }
@@ -20,10 +22,17 @@ class BillerCode1034 implements BillerCdvInterface
 
     private function validateLength($mainField) {
         $length = strlen($mainField);
-        return ($length >= 8 && $length <= 20) ? true : false;
+        return ($length >= 8 && $length <= 9) ? true : false;
     }
 
     private function validateCharacters($mainField) {
-        return is_numeric($mainField);
+        return ctype_alnum($mainField);
+    }
+
+    private function validateFormat($mainField) {
+        $firstToEightDigit = substr($mainField, 0, 8);
+        $nineDigit = substr($mainField, -1);
+
+        return is_numeric($firstToEightDigit) && ctype_alnum($nineDigit) ? true : false;
     }
 }
